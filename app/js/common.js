687cd735b34c6da11ee3ec4194724c1d3bfbd83a
Mihail Rachev <webgame72@gmail.com>
2023-07-29 03:30:48 +0500

    Removed unnecessary elements in the assembly

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,1 @@
-$(function () {
-    // $('[data-action=open-modal]').click(function() {
-    //     const modal = $(`${$(this).attr('data-modal')}.modal`);
-    //     $(modal).toggleClass('active');
-    // });
-
-    // $('.modal').click(function(e) {
-    //     if ($('.modal-inner-block').has(e.target).length < 1) {
-    //         $('.modal').removeClass('active');
-    //     }
-    // });
-
-    // $('[data-action=modal-close]').click(function() {
-    //     $('.modal').removeClass('active');
-    // });
-});
+$(function () {});
